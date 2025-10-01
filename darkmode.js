0520e91ab37bb0641019ac8beb5d896cdf0594f5
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-10-01 13:09:12 +0100

    dark btn switch

--- darkmode.js
+++ darkmode.js
@@ -1,15 +1,27 @@
 let darkmode = localStorage.getItem('darkmode');
+let lightmode = localStorage.getItem('lightmode');
+
 
 const themeSwitch = document.getElementById('theme-switch');
 
-const enableDarkmode = () => {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkmode','active');
+// const enableDarkmode = () => {
+//     document.body.classList.add('darkmode');
+//     localStorage.setItem('darkmode','active');
+// }
+
+// const disableDarkmode = () => {
+//     document.body.classList.remove('darkmode');
+//     localStorage.setItem('darkmode',null);
+// }
+
+const enableLightmode = () => {
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode','active');
 }
 
-const disableDarkmode = () => {
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkmode',null);
+const disableLightmode = () => {
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('lightmode',null);
 }
 
 if(darkmode === 'active') enableDarkmode();
